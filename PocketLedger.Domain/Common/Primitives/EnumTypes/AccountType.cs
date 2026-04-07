41fa2d260512ef99362eae620d7abd6e30c63fce
Juan Ochoa <juan8apablo@gmail.com>
2026-04-07 12:15:35 -0500

    Add Account Entity

---
+++ PocketLedger.Domain/Common/Primitives/EnumTypes/AccountType.cs
@@ -0,0 +1,26 @@
+﻿namespace PocketLedger.Domain.Common.Primitives.EnumTypes;
+
+public enum AccountTypeValue
+{
+    Cash = 1,
+    Checking = 2,
+    Savings = 3,
+    CreditCard = 4,
+    DigitalWallet = 5
+}
+
+public readonly record struct AccountType(AccountTypeValue Value) : IPrimitiveValue<AccountTypeValue>
+{
+    public object RawValue => Value;
+
+    public static AccountType Cash => new(AccountTypeValue.Cash);
+    public static AccountType Checking => new(AccountTypeValue.Checking);
+    public static AccountType Savings => new(AccountTypeValue.Savings);
+    public static AccountType CreditCard => new(AccountTypeValue.CreditCard);
+    public static AccountType DigitalWallet => new(AccountTypeValue.DigitalWallet);
+
+    public override string ToString() => Value.ToString();
+
+    public static implicit operator AccountTypeValue(AccountType value) => value.Value;
+    public static implicit operator AccountType(AccountTypeValue value) => new(value);
+}
---
+++ PocketLedger.Domain/Ledger/Account.cs
@@ -0,0 +1,61 @@
+﻿using LanguageExt;
+using PocketLedger.Domain.Common.ErrorTypes;
+using static LanguageExt.Prelude;
+using PocketLedger.Domain.Common.Primitives.EnumTypes;
+using PocketLedger.Domain.Common.Primitives.GuidTypes;
+using PocketLedger.Domain.Common.Primitives.NumericTypes;
+using PocketLedger.Domain.Common.Primitives.StringTypes;
+
+namespace PocketLedger.Domain.Ledger;
+
+public sealed record Account(
+    AccountId Id,
+    Name Name,
+    AccountType Type,
+    Currency Currency,
+    Option<Money> InitialBalance,
+    bool IsActive,
+    DateTimeOffset CreatedAt,
+    Option<DateTimeOffset> UpdatedAt)
+{
+    public static Validation<Error, Account> Create(
+        AccountId id,
+        Name name,
+        AccountType type,
+        Currency currency,
+        Option<Money> initialBalance,
+        bool isActive,
+        DateTimeOffset createdAt,
+        Option<DateTimeOffset> updatedAt) =>
+        Success<Error, Account>(
+            new Account(
+                id,
+                name,
+                type,
+                currency,
+                initialBalance,
+                isActive,
+                createdAt,
+                updatedAt));
+
+    public Account Deactivate(DateTimeOffset updatedAt) =>
+        this with
+        {
+            IsActive = false,
+            UpdatedAt = Some(updatedAt)
+        };
+
+    public Account Activate(DateTimeOffset updatedAt) =>
+        this with
+        {
+            IsActive = true,
+            UpdatedAt = Some(updatedAt)
+        };
+
+    public Account UpdateName(Name name, DateTimeOffset updatedAt) =>
+        this with
+        {
+            Name = name,
+            UpdatedAt = Some(updatedAt)
+        };
+}
