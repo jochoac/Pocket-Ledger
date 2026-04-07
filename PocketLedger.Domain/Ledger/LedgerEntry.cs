690213111e8ccff4067adb2ee43192f93220b474
Juan Ochoa <juan8apablo@gmail.com>
2026-04-07 11:42:37 -0500

    Create method for LedgerEntry Entity

--- PocketLedger.Domain/Ledger/LedgerEntry.cs
+++ PocketLedger.Domain/Ledger/LedgerEntry.cs
@@ -1,7 +1,10 @@
 ﻿using LanguageExt;
+using PocketLedger.Domain.Common.ErrorTypes;
 using PocketLedger.Domain.Common.Primitives;
 
 namespace PocketLedger.Domain.Ledger;
+
+using static LanguageExt.Prelude;
 
 public sealed record LedgerEntry(
     LedgerEntryId Id,
@@ -10,11 +13,65 @@ public sealed record LedgerEntry(
     Option<SourceEventId> SourceEventId,
     TransactionType TransactionType,
     Money Amount,
-    CurrencyCode Currency,
+    CurrencyCode CurrencyCode,
     EntryDescription Description,
     Option<MerchantName> MerchantName,
     Option<ExternalReference> ExternalReference,
     DateTimeOffset OccurredAt,
-    DateTimeOffset CreatedAt
-);
+    DateTimeOffset CreatedAt)
+{
+    public static Validation<Error, LedgerEntry> Create(
+        LedgerEntryId id,
+        AccountId accountId,
+        Option<CategoryId> categoryId,
+        Option<SourceEventId> sourceEventId,
+        TransactionType transactionType,
+        Money amount,
+        CurrencyCode currencyCode,
+        EntryDescription description,
+        Option<MerchantName> merchantName,
+        Option<ExternalReference> externalReference,
+        DateTimeOffset occurredAt,
+        DateTimeOffset createdAt)
+    {
+        var categoryRules =
+            transactionType.Value switch
+            {
+                TransactionTypeValue.Expense when categoryId.IsNone =>
+                    Errors.InvalidValue<Unit>(
+                        nameof(CategoryId),
+                        "Expense transactions require a category."),
 
+                TransactionTypeValue.Transfer when categoryId.IsSome =>
+                    Errors.InvalidValue<Unit>(
+                        nameof(CategoryId),
+                        "Transfer transactions must not have a category."),
+
+                _ => Success<Error, Unit>(unit)
+            };
+
+        var dateRules =
+            occurredAt > createdAt
+                ? Errors.InvalidValue<Unit>(
+                    nameof(OccurredAt),
+                    "OccurredAt cannot be after CreatedAt.")
+                : Success<Error, Unit>(unit);
+
+        return
+            from _1 in categoryRules
+            from _2 in dateRules
+            select new LedgerEntry(
+                id,
+                accountId,
+                categoryId,
+                sourceEventId,
+                transactionType,
+                amount,
+                currencyCode,
+                description,
+                merchantName,
+                externalReference,
+                occurredAt,
+                createdAt);
+    }
+}
